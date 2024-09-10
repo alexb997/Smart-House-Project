c710f53e08e06100c274d27f9addb32599b5d56a
alexb997 <alex_alexandru_997@yahoo.ro>
2024-09-10 21:23:04 +0300

    cors fix

--- smarthome-frontend/src/axios.js
+++ smarthome-frontend/src/axios.js
@@ -1,18 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: "http://localhost:8080",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-});
-
-instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default instance;
