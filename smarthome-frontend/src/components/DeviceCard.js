4b6d2258e56188dd6874a88396af839c435fdff5
alexb997 <alex_alexandru_997@yahoo.ro>
2024-11-07 11:01:24 +0200

    device reacting to environment

--- smarthome-frontend/src/components/DeviceCard.js
+++ smarthome-frontend/src/components/DeviceCard.js
@@ -3,6 +3,7 @@ import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 import CustomButton from "./CustomButton";
 import DeviceDetails from "./DeviceDetails";
+import { createDeviceLog } from "../service/logService";
 
 const DeviceCard = ({ device, manage, remove }) => {
   const cardStyle = {
@@ -11,13 +12,66 @@ const DeviceCard = ({ device, manage, remove }) => {
     marginBottom: "20px",
   };
 
+  const generateDeviceLog = (deviceType, deviceName, username) => {
+    let message;
+    let status;
+  
+    switch (deviceType) {
+      case "CAMERA":
+        message = "Camera observed movement";
+        status = "OBSERVE";
+        break;
+      case "DOORBELL":
+        message = "Doorbell rang";
+        status = "RING";
+        break;
+      case "LOCK":
+        message = "Lock engaged or disengaged";
+        status = "LOCK";
+        break;
+      case "SENSOR":
+        message = "Sensor detected a change in environment";
+        status = "SENSE";
+        break;
+      default:
+        message = "Unknown device activity";
+        status = "UNKNOWN";
+    }
+  
+    const log = {
+      deviceName: deviceName || "Unnamed Device",
+      date: new Date().toISOString(),
+      message,
+      status,
+      username,
+    };
+  
+    console.table(log);
+    return log;
+  }
+
+  const handleDeviceAction = async (device) => {
+    const username = localStorage.getItem("username");
+    const log = generateDeviceLog(device.type, device.name, username);
+  
+    try {
+      await createDeviceLog(log);
+      console.log("Log created successfully.");
+    } catch (error) {
+      console.error("Error creating log:", error);
+    }
+  };
+
   return (
     <Card style={cardStyle} className="device-card">
       <Card.Body>
         <Card.Title style={{ textAlign: "center" }}>
           {device.name || "Unnamed Device"}
         </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
+        <Card.Subtitle
+          className="mb-2 text-muted"
+          onClick={() => handleDeviceAction(device)}
+        >
           Type: {device.type}
         </Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted">
